442950fbbec45bab4dad43f7e7e3c44bc1ab3414
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-13 10:30:09 +0330

    start technoprint

---
+++ index.php
@@ -0,0 +1,12 @@
+<!doctype html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Technoprint</title>
+	<link rel="stylesheet" type="text/css" href="./1styles.css" />
+	<link rel="stylesheet" type="text/css" href="./style.css" />
+</head>
+<body>
+	
+</body>
+</html>
