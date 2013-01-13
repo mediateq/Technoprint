e37dd5bb6568699a3fa9e39b6ae32ef331d472b2
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-13 12:47:01 +0330

    create layout2

--- index.php
+++ index.php
@@ -7,27 +7,57 @@
 	<link rel="stylesheet" type="text/css" href="./style.css" />
 </head>
 <body>
-	<!-- Header -->
-	<header>
-		<div class="top-header">
-			<div class="container"></div>
-		</div>
-		<div class="main-header">
-			<div class="container"></div>
-		</div>
-	</header>
-	<!-- Middle -->
-	<section class="middle">
-		<div class="container"></div>
-	</section>
-	<!-- footer -->
-	<footer>
-		<div class="main-footer">
-			<div class="container"></div>
-		</div>
-		<div class="bot-footer">
-			<div class="container"></div>
-		</div>
-	</footer>
+	<div class="contain">
+		<!-- Header -->
+		<header>
+			<!-- Top Header -->
+			<div class="top-header">
+				<div class="container">
+					<div class="menu"></div>
+					<div class="left"></div>
+					<div class="badboy"></div>
+				</div>
+			</div>
+			<!-- Main Header -->
+			<div class="main-header">
+				<div class="container">
+					<div class="menu"></div>
+					<div class="logo"></div>
+					<div class="badboy"></div>
+				</div>
+			</div>
+		</header>
+		<!-- Middle -->
+		<section class="middle">
+			<div class="container">
+				<div class="slideshow"></div>
+				<div class="hline"></div>
+				<div class="text"></div>
+				<div class="hline"></div>
+				<div class="products"></div>
+				<div class="client"></div>
+			</div>
+		</section>
+		<!-- footer -->
+		<footer>
+			<!-- Main Footer -->
+			<div class="main-footer">
+				<div class="container">
+					<div class="first"></div>
+					<div class="second"></div>
+					<div class="third"></div>
+					<div class="forth"></div>
+				</div>
+			</div>
+			<!-- Bot Footer -->
+			<div class="bot-footer">
+				<div class="container">
+					<div class="top"></div>
+					<div class="copyright"></div>
+					<div class="menu"></div>
+				</div>
+			</div>
+		</footer>
+	</div>
 </body>
 </html>
