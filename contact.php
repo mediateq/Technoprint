2b5c7bba35412d40c158d22159a1c8307bc8deb6
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-23 11:06:04 +0330

    add images of title to others page

--- contact.php
+++ contact.php
@@ -1,5 +1,13 @@
 <?php include('./inc/header.php') ?>
 
-
+<section class="contact">
+	<div class="container">
+		<div id="title-pages">
+			<div class="top-pic"></div>
+			<div class="main-pic"></div>
+			<div class="bot-pic"></div>
+		</div>
+	</div>
+</section>
 
 <?php include('./inc/footer.php') ?>
