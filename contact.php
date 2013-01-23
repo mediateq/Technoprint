a952741085a1d5bb527f56489c7223a008a53a20
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-23 12:22:07 +0330

    add title of others page & menu

--- contact.php
+++ contact.php
@@ -3,9 +3,24 @@
 <section class="contact">
 	<div class="container">
 		<div id="title-pages">
-			<div class="top-pic"></div>
-			<div class="main-pic"></div>
-			<div class="bot-pic"></div>
+			<div id="top-pic"></div>
+			<div id="main-pic">
+				<div id="title">
+					<p>تماس با ما</p>
+				</div>
+				<div id="search"></div>
+				<div id="menu">
+					<nav>
+						<menu>
+							<li><a href="#"><p>تماس با ما</p></a><span></span></li>
+							<li><a href="#"><p>جفنگ</p></a><span></span></li>
+							<li><a href="#"><p>خانه</p></a><span></span></li>
+						</menu>
+					</nav>
+				</div>
+				<div class="badboy"></div>
+			</div>
+			<div id="bot-pic"></div>
 		</div>
 	</div>
 </section>
