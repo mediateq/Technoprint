f69b1f7d4a3356f8554abb729e310d95ddefe4d4
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-23 20:18:34 +0330

    add map to contact page

--- contact.php
+++ contact.php
@@ -41,12 +41,18 @@
 					<input type="text" class="text" name="email" id="email" autocomplete="off" />
 					<p><span>*</span> پیام</p>
 					<textarea name="message" id="message" cols="25" rows="5" ></textarea>
-					<p>.پر کردن فیلدهای <span>*</span> دار الزامیست</p>
+					<p>.پر کردن فیلدهای <span>ستاره دار</span> الزامیست</p>
 					<input type="submit" class="button" id="submit" name="submit" value="ارسال" />
 				</form>
 			<div class="badboy"></div>
 			</div>
-			<div class="detail"></div>
+			<div class="detail">
+				<div class="phone-pic">
+					<div class="map">
+						<iframe width="211" height="322" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps/ms?msa=0&amp;msid=208152577024565193217.0004c1c95d80b1d14a8b2&amp;hl=en&amp;ie=UTF8&amp;t=m&amp;ll=36.313362,59.561949&amp;spn=0.011135,0.009012&amp;z=15&amp;output=embed"></iframe>
+					</div>
+				</div>
+			</div>
 			<div class="badboy"></div>
 		</div>
 	</div>
