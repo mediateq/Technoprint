bde41e88bc17c49da361c0756c675e47cac99af9
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-23 22:03:19 +0330

    add text and menu to 404 page

--- 404.php
+++ 404.php
@@ -31,7 +31,19 @@
 		</div>
 		<!-- end Title of each page -->
 		<div class="notfound-404">
-			<div class="detail"></div>
+			<div class="detail">
+				<div class="tit"><p>صفحه مورد نظر یافت نشد</p></div>
+				<div class="text"><p>صفحه ای که شما به دنبال آن می باشید متاسفانه یافت نشد. برخی از لینک هایی که ممکن است برای شما مفید واقع شوند</p></div>
+				<div class="menu">
+					<nav>
+						<menu>
+							<li><a href="#">صفحه اصلی</a></li>
+							<li><a href="#">نقشه سایت</a></li>
+							<li><a href="#">تماس با ما</a></li>
+						</menu>
+					</nav>
+				</div>
+			</div>
 			<div class="pic"></div>
 		</div>
 	</div>
