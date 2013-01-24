345544862237db5fd82d94f6d1a66d825359f52c
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-24 18:01:21 +0330

    start to create page for one produce

---
+++ product-intro.php
@@ -0,0 +1,60 @@
+<?php include('./inc/header.php') ?>
+
+<section class="sub-products">
+	<div class="container">
+		<!-- start Title of each page -->
+		<div id="title-pages">
+			<div id="top-pic"></div>
+			<div id="main-pic">
+				<div id="title">
+					<h2>اسکنر</h2>
+				</div>
+				<div id="search">
+					<div class="search-box">
+						<form action="">
+							<input type="text" class="text" placeholder="جستجو" />
+							<input type="submit" value="" class="submit" />
+						</form>
+					</div>
+				</div>
+				<div id="menu">
+					<nav>
+						<menu>
+							<li><a href="#"><p>اسکنر</p></a><span></span></li>
+							<li><a href="#"><p>محصولات</p></a><span></span></li>
+							<li><a href="#"><p>صفحه اصلی</p></a><span></span></li>
+						</menu>
+					</nav>
+				</div>
+				<div class="badboy"></div>
+			</div>
+			<div id="bot-pic"></div>
+		</div>
+		<!-- end Title of each page -->
+		<div class="one-produce">
+			<div class="produce">
+
+			</div>
+			<!-- Start Categories of product -->
+			<div id="cat">
+				<div class="vline"></div>
+				<h2>گروه بندی محصولات</h2>
+				<nav>
+					<menu>
+						<li><a href="#">اسکنر</a></li>
+						<li><a href="#">موس</a></li>
+						<li><a href="#">پرینتر</a></li>
+						<li><a href="#">دوربین</a></li>
+						<li><a href="#">کی بورد</a></li>
+					</menu>
+				</nav>
+				<div class="hline"></div>
+				<div class="badboy"></div>
+			</div>
+			<!-- End Categories of product -->
+			<div class="badboy"></div>
+		</div>
+	</div>
+</section>
+
+<?php include('./inc/footer.php') ?>
