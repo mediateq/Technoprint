acbecd9d02487f35d85b08cb08dc82bea47962eb
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-01-26 14:10:20 +0330

    strat to create about us page

---
+++ about.php
@@ -0,0 +1,37 @@
+<?php include('./inc/header.php') ?>
+
+<section class="about-us">
+	<div class="container">
+		<!-- start Title of each page -->
+		<div id="title-pages">
+			<div id="top-pic"></div>
+			<div id="main-pic">
+				<div id="title">
+					<h2>درباره ما</h2>
+				</div>
+				<div id="search">
+					<div class="search-box">
+						<form action="">
+							<input type="text" class="text" placeholder="جستجو" />
+							<input type="submit" value="" class="submit" />
+						</form>
+					</div>
+				</div>
+				<div id="menu">
+					<nav>
+						<menu>
+							<li><a href="#"><p>درباره ما</p></a><span></span></li>
+							<li><a href="#"><p>صفحه اصلی</p></a><span></span></li>
+						</menu>
+					</nav>
+				</div>
+				<div class="badboy"></div>
+			</div>
+			<div id="bot-pic"></div>
+		</div>
+		<!-- end Title of each page -->
+		<div class="about"></div>
+	</div>
+</section>
+
+<?php include('./inc/footer.php') ?>
