f32c6c20ab982b50524a7dbf51295b7d6f6580a5
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-11 12:17:57 +0330

    add taxonomy for product

---
+++ taxonomy.php
@@ -0,0 +1,62 @@
+<?php 
+	get_template_part('./inc/header');
+	$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
+?>
+
+<section class="sub-products">
+	<div class="container">
+		<!-- start Title of each page -->
+		<div id="title-pages">
+			<div id="top-pic"></div>
+			<div id="main-pic">
+				<div id="title">
+					<h2><?php echo apply_filters( 'the_title', $term->name ); ?></h2>
+				</div>
+				<div id="search">
+					<div class="search-box">
+						<form action="">
+							<input type="text" class="text" placeholder="جستجو" />
+							<input type="submit" value="" class="submit" />
+						</form>
+					</div>
+				</div>
+				<div id="menu">
+					<nav>
+						<?php
+							wp_nav_menu( array(
+								'theme_location'  => 'titlemenu',
+								'menu'            => '', 
+							    'container'       => false, 
+							    'container_class' => '', 
+							    'menu_class'      => '',
+							    'echo'            => true, 
+							    'fallback_cb'     => 'wp_page_menu',
+							    'before'          => '',
+							    'after'           => '',
+							    'link_before'     => '<p>',
+							    'link_after'      => '</p><span></span>',
+							    'items_wrap'      => '<menu>%3$s</menu>',
+							    'depth'           => 0,
+							    'walker'          => ''
+							));
+						?>
+					</nav>
+				</div>
+				<div class="badboy"></div>
+			</div>
+			<div id="bot-pic"></div>
+		</div>
+		<!-- end Title of each page -->
+		<div class="all-products">
+			<div class="product">
+								
+				<!-- Start Categories of product -->
+				<?php get_template_part('./inc/product-cat'); ?>
+				<!-- End Categories of product -->
+				<div class="badboy"></div>
+			</div>
+		</div>
+	</div>
+</section>
+
+<?php get_template_part('./inc/footer') ?>
