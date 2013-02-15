3705b3bf49d58014d708399aeedbd798bec445c6
Mojtaba Amjadi <amjadi.mojtaba@gmail.com>
2013-02-15 23:26:51 +0330

    fix taxonomy

--- taxonomy.php
+++ taxonomy.php
@@ -3,7 +3,7 @@
 	$term = get_term_by( 'slug', get_query_var( 'term' ), get_query_var( 'taxonomy' ) );
 ?>
 
-<section class="sub-products">
+<section class="products">
 	<div class="container">
 		<!-- start Title of each page -->
 		<div id="title-pages">
@@ -49,12 +49,26 @@
 		<!-- end Title of each page -->
 		<div class="all-products">
 			<div class="product">
-								hlhlhljklhjl
+					<ul>
+						<?php
+							while(have_posts()){
+								the_post();
+						?>
+							<li>
+								<a href="<?php the_permalink(); ?>">
+									<?php the_post_thumbnail(); ?>
+									<h2 class='product-name'><?php the_title(); ?></h2>
+								</a>
+							</li>
+						<?php 
+							}
+						?>
+					</ul>
+			</div>
 				<!-- Start Categories of product -->
 				<?php get_template_part('./inc/product-cat'); ?>
 				<!-- End Categories of product -->
 				<div class="badboy"></div>
-			</div>
 		</div>
 	</div>
 </section>
